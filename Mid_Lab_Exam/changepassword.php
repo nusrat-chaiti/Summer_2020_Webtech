557c95967d4ccbb12cd95b7c4f60cae61e0e5a0f
nusrat-chaiti <47456761+nusrat-chaiti@users.noreply.github.com>
2020-08-04 09:25:25 +0600

    mid lab exam

---
+++ Mid_Lab_Exam/changepassword.php
@@ -0,0 +1,38 @@
+<?php
+	session_start();
+
+	if(isset($_POST['submit'])){
+
+		
+		        $np 	= $_POST['np'];
+        		$np1 	= $_POST['np1'];
+        		$np2	= $_POST['np2'];
+
+
+        
+
+		if(empty($np) || empty($pnp1) ||  empty($np2) ){
+			echo "null values in password field";
+
+		}else if(isset($_COOKIE['password'])){
+			
+			if($np == $_COOKIE['password']){
+				
+
+				setcookie('status', "OK", time()+3600, '/');
+				header('location: change_password.html');
+
+			}else{
+				echo "password not valid";
+			}
+			
+		}else{
+			echo "Not found!";
+		}
+
+	}else{
+		header('location: changepassword.html');
+	}
+
+
+?>
