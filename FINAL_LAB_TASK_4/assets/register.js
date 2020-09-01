92ea8bae0e9722c844fe29fe46fef04575cc3990
nusrat-chaiti <47456761+nusrat-chaiti@users.noreply.github.com>
2020-09-01 09:40:33 +0600

    full js validation done

--- FINAL_LAB_TASK_4/assets/register.js
+++ FINAL_LAB_TASK_4/assets/register.js
@@ -40,6 +40,90 @@ function nremover()
     document.getElementById('name_Msg').innerHTML = "";
     
 }
+//// email check
+
+
+function empty()
+{
+    var email = document.getElementById("email").value;
+   
+
+    if(email=="")
+    {
+       document.getElementById("email_Msg").innerHTML=" email field can't be empty";
+    }
+    
+    
+    else
+    {
+        var count=0;
+		for( i=0 ; i < email.length ; i++)
+		{
+			if(email[i]=='@')
+			{
+				count++;
+			}
+			else
+				continue;
+		}
+		if(count==1)
+		{
+			var at_the_ret = email.split("@");
+			var dot = at_the_ret[1].split(".");
+			var last;
+			for( i=0 ; i < dot.length ; i++)
+			{
+				last = dot[i];
+			}
+			if( last == "com" || last == "edu"|| last=="org")
+			{
+				return true;
+			}
+			else
+			{
+                document.getElementById("email_Msg").innerHTML=" enter valid email";
+
+			}
+        }
+        else
+        {
+            document.getElementById("email_Msg").innerHTML=" enter valid email";
+
+        }
+    }
+}
+
+function eremover()
+{
+    document.getElementById("email_Msg").innerHTML="";
+}
+
+///validation of pass
+
+
+function pempty()
+{ 
+    var password = document.getElementById("password").value;
+
+    if(password=="")
+    {
+        document.getElementById("pass_Msg").innerHTML="password field can't be empty";
+
+    }
+
+    else
+    {
+         return true;
+       
+    }
+}
+
+function premover()
+{
+    document.getElementById("pass_Msg").innerHTML="";
+}
+
+
 
  
 
@@ -50,4 +134,8 @@ function Validate()
       return true;
     
     
+}
+function f1()
+{
+    document.getElementsByTagName('a')[0].style.display = 'inline';
 }
--- FINAL_LAB_TASK_4/views/register.php
+++ FINAL_LAB_TASK_4/views/register.php
@@ -7,26 +7,38 @@
 </head>
 <body>
 
-	<form action="#" method="post">
+	<form action="#" onsubmit="return Validate()"  method="post">
 		<fieldset>
 			<legend>SignUp</legend>
 			<table>
 				<tr>
 					<td>Username</td>
-					<td><input type="text" name="name"  onkeyup="nremover()" onblur="nempty()" ><b><i id="name_Msg"></i></b></td>
+					<td><input type="text" name="name" id="name" onkeyup="nremover()" onblur="nempty()" >
+                    <b><i id="name_Msg"></i></b><br>
+
+                    </td>
 				</tr>
                 <tr>
 					<td>Email</td>
-					<td><input type="text" name="email" onkeyup="eremover()" onblur="empty()"></td>
+					<td><input type="text" id="email" name="email"  onkeyup="eremover()" onblur="empty()">
+                    
+                    <b><i id="email_Msg"></i></b><br>
+                    </td>
+
 				</tr>
 				<tr>
 					<td>Password</td>
-					<td><input type="password" name="password" onkeyup="premover()" onblur="pempty()"></td>
+					<td><input type="password" id = "password" name="password" onkeyup="premover()" onblur="pempty()">
+                    <b><i id="pass_Msg"></i></b><br>
+
+                    </td>
 				</tr>
 				
 				<tr>
 					<td></td>
-					<td><input type="button" name="submit" value="Submit"></td>
+					<td><input type="button" name="submit" value="Submit" onclick="f1()" >
+                    <a href="login.php" style="display: none">login</a>
+                   </td>
 				</tr>
 			</table>
 		</fieldset>
