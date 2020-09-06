4ce4732610118dccf94a31a3be957bbcd2772cb0
nusrat-chaiti <47456761+nusrat-chaiti@users.noreply.github.com>
2020-09-06 08:47:27 +0600

    login and registration form created

---
+++ FINAL_LAB_EXAM/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home page</title>
+</head>
+<body>
+    <table width=100% border='1'>
+      <tr align="center">
+        <td colspan="2">
+           welcome <br>
+            
+        </td>
+      </tr>
+      <tr>
+        <td >
+           <a href="views/login.php">login <br>
+            
+        </td>
+        <td>
+           <a href="views/registration.php">Registration<br>
+        </td>
+      </tr>
+    </table>
+</body>
+</html>
---
+++ FINAL_LAB_EXAM/php/session_header.php
@@ -0,0 +1,9 @@
+<?php
+	session_start();
+
+	if(!isset($_SESSION['username'])){
+		header('location: ../views/login.php?error=invalid_request');
+	}
+	
+	
+?>
